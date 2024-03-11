c12584a1075f5894367ba402da66b30a922f7f0b
sahilsindwal <sahilsindwal@gmail.com>
2024-03-11 22:27:44 +0530

    -

---
+++ permi.js
@@ -0,0 +1,34 @@
+// cv permissions code
+
+let win = document.querySelector('.cvPermissions');
+
+function closeWindow() {
+    win.style.cssText = 'display: none;';
+}
+
+
+function cvWindow() {
+    win.style.cssText = 'display: block;';
+}
+
+function getVal() {
+    let token = document.querySelector('#token');
+    let cvbtn = document.querySelector('#cvbtn');
+    let cvsub = document.querySelector('.cvsub');
+
+    if (token.value == 512) {
+
+        let Enable = document.querySelector('#allow');
+        Enable.style.cssText = 'display: none;';
+        token.style.cssText = 'display: none;';
+        cvsub.style.cssText = 'display: none;';
+        cvbtn.style.cssText = 'display: block;';
+        cvText.style.cssText = 'color:green';
+
+    }
+
+    else {
+        cvText.style.cssText = 'color:red';
+        cvText.textContent = `you don't have permissions`;
+    }
+}
